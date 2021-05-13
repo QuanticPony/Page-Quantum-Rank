c4c8e4ceb28f78b9f2f89188d0912c47484f131d
Unai Lería Fortea <unicelularuni15@gmail.com>
2021-05-13 16:28:50 +0200

    Añadir fichero funciones.py

---
+++ funciones.py
@@ -0,0 +1,125 @@
+from collections import defaultdict
+from time import time_ns as time
+
+import numpy as np
+from numba import jit
+from numba.np.ufunc import parallel
+
+
+def read_links(filename, n_nodes):
+    '''
+    Reads a file and returns the adjacency matrix
+    '''
+    cero = lambda : 0
+    A_ij = [defaultdict(cero) for _ in range(n_nodes)]
+    A_ji = [defaultdict(cero) for _ in range(n_nodes)]
+
+    with open(filename, 'r') as file:
+        try:
+            for line in file:
+                i,j,p = map(float,line.split())
+                i, j = int(i), int(j)
+                A_ij[i].update({j:A_ij[i][j]+p})
+        except:
+            file.seek(0,0)
+            for line in file:
+                i,j = map(int, line.split())
+                A_ij[i].update({j:1})
+    return A_ij
+
+def transpuesta(b_ij):
+    cero = lambda : 0
+    lenth = len(b_ij)
+    b_ji = [defaultdict(cero) for _ in range(lenth)]
+    for i, b_i in enumerate(b_ij):
+        for j in b_i.keys():
+            b_ji[j].update({i: b_i[j]})
+    return b_ji
+
+def calculate_PI(a_ij):
+    '''
+    Given the adjacency matrix returns the transition matrix
+    '''
+    n_nodes = len(a_ij)
+    cero = lambda : 0
+    PI_ij = [defaultdict(cero) for _ in range(n_nodes)]
+    
+    for i, ai in enumerate(a_ij):
+        PI_ij[i].update(ai.copy())
+        
+        out_grade = sum(ai.values())
+        for j in PI_ij[i].keys():
+            PI_ij[i][j] /= out_grade    
+    return PI_ij
+
+def evolve(P, PI_ij,  **kargs):
+    '''
+    Evolves the system a temporal discrete step using the transition matrix
+    '''
+    Pnew=np.zeros(len(P))
+    for i in range(len(P)):
+        for j in range(len(P)):
+            Pnew[i] += P[j] * PI_ij[j][i]
+    return Pnew     
+
+
+def evolve_T(P, PI_ji,  **kargs):
+    '''
+    Evolves the system a temporal discrete step using the transition matrix
+    '''
+    Pnew=np.zeros(len(P))
+    for i, PI_i in enumerate(PI_ji):
+        for j, p in PI_i.items():
+            Pnew[i] += P[j] * p
+    return Pnew     
+
+def evolveG(P, PI_ij, q=0.9):
+    '''
+    Evolves the system a temporal discrete step using the Google matrix
+    '''
+    F = (1-q)/(len(P)-1)
+    Pnew=np.zeros(len(P))
+    for i in range(len(P)):
+        for j in range(len(P)):
+            Pnew[i] += P[j] * (q * PI_ij[j][i]+ (i!=j)*F)
+    return Pnew     
+
+    
+def equal(P1,P2, delta):
+    '''
+    Returns `True` if all the components in `P1` and `P2` are closer than `delta`. Else returns `False`
+    '''
+    for i,j in zip(P1,P2):
+        if abs(i-j)>delta:
+            return False      
+    return True
+
+
+def print_rank(P):
+    '''
+    Prints out via terminal the nodes and the page rank given `P`
+    '''
+    print('Pagerank:')
+    t = np.argsort(P)[::-1]
+    for i in t:
+        print(i+1, P[i])
+        
+        
+def page_rank(a_ij, evolve_func, equal_func, *, q=0.9, P=None, max_iters=1000, delta=0.000001, _transpuesta=False):
+        pi_ij = calculate_PI(a_ij)
+        if _transpuesta:
+            pi_ij = transpuesta(pi_ij)
+            
+        if P is None:
+            P = np.zeros(8)
+            P[0]=1
+        
+        time_in = float(time())
+        for _ in range(max_iters):
+            Pnew = evolve_func(P, pi_ij, q=q)
+            if equal(P, Pnew, delta):
+                break
+            P = evolve_func(Pnew, pi_ij)
+        print(f'Tiempo transcurrido: {(time()-time_in)*1e-6:.6f}ms')
+        P=P/sum(P)
+        print_rank(P)
