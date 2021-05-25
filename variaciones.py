8230a9e24904e13ce6a99c1ecc54daa53a66e59e
Unai Lería Fortea <unicelularuni15@gmail.com>
2021-05-25 20:51:08 +0200

    Corregir colores

--- variaciones.py
+++ variaciones.py
@@ -9,6 +9,8 @@ from pyvis.network import Network
 
 
 from matplotlib import pyplot as plt
+from matplotlib import colors
+from matplotlib import cm
 
 import pickle
 
@@ -44,19 +46,37 @@ for style in styles:
         
 serie = np.zeros(N_samples)  
 
+
 for style in styles:
     fig, ax = plt.subplots()
     
-    ax.set_title(style)
-    ax.set_xlabel(r'$\alpha$')
-    ax.set_ylabel('QR')
+    ax.grid()
     
-    ax.set_ylim(0,0.2)
+    style_types = {
+        'mean': r'$(g_{ij}+g_{ji})/2$',
+        'max': r'$max(g_{ij},g_{ji})$',
+        'min': r'$min(g_{ij},g_{ji})$',
+        'norm': r'$\sqrt{g_{ij}^2+g_{ji}^2}$',
+        'diff': r'$|g_{ij}-g_{ji}|$',
+    }
+    
+    ax.set_title(f"Hamiltonian: {style_types[style]}")
+    ax.set_xlabel(r'$\alpha$')
+    ax.set_ylabel('Quantum Rank')
+    
+    ax.set_ylim(0,0.16)
     ax.set_xlim(0.1,1)
+    norm = colors.Normalize(vmin=min(normal_rank[style][0]), vmax=max(normal_rank[style][0]))
+    cmap = cm.get_cmap('rainbow')
+    
+    def color(value):
+        rgb = cmap(norm(abs(value)))[:3]
+        color = colors.rgb2hex(rgb)
+        return color
     
     for i in range(N_nodos):
         for j, alpha in enumerate(alpha_range):
             serie[j] = quantum_rank[style][j][i]
-        ax.plot(alpha_range, serie)
+        ax.plot(alpha_range, serie, color=color(normal_rank[style][0][i]))
         
 plt.show()
