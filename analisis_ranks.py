0ad875ad5124377cf64c54a6d310d93404b6e19f
Unai Lería Fortea <unicelularuni15@gmail.com>
2021-05-25 17:46:09 +0200

    Añadir fit a potencial

--- analisis_ranks.py
+++ analisis_ranks.py
@@ -11,6 +11,8 @@ from matplotlib import pyplot as plt
 
 import pickle
 
+from scipy.optimize import curve_fit
+
 
 with open('datos.dat', 'rb') as file:
     PR, QR, GR, names = pickle.load(file)
@@ -30,6 +32,7 @@ with open('datos.dat', 'rb') as file:
 #print(p_GR)
 #print(p_QR)
 
+#%%
 
 #plt.scatter(np.arange(0,41), p_QR-p_PR)
 
@@ -58,9 +61,10 @@ plt.show()
 
 #%%
 
+fig2, ax2 = plt.subplots()
+
 A_ij = read_links('links-sin-bots.txt', 41)
 TA_ij = transpuesta(A_ij)
-
 
 Grado_in = np.zeros(41)
 Grado_out = np.zeros(41)
@@ -72,8 +76,6 @@ for i, tai in enumerate(TA_ij):
 P_grado_in = Grado_in / sum(Grado_in)
 P_grado_out = Grado_out / sum(Grado_out)
 
-plt.show()
-#%%
 NNN = 4
 
 range_in = np.linspace(0,max(Grado_in)+0.001, NNN)
@@ -83,23 +85,43 @@ delta_out = range_out[1]-range_out[0]
 
 P_grado_in = np.zeros(NNN-1)
 P_grado_out = np.zeros(NNN-1)
+
 for gin, gout in zip(Grado_in, Grado_out):
     P_grado_in[int(np.floor(gin/delta_in))] += 1
     P_grado_out[int(np.floor(gout/delta_out))] += 1
     
 P_grado_in /= sum(P_grado_in)
 P_grado_out /= sum(P_grado_out)
+
+
+def exponential(x, m, n):
+    return pow(x, m) + n
+
+popt_in, pcov_in = curve_fit(exponential, range_in[:-1]+delta_in/2, P_grado_in, p0=[-3,0])
+popt_out, pcov_out = curve_fit(exponential, range_out[:-1]+delta_out/2, P_grado_out, p0=[-3,0])
+
+in_range = range_in[:-1]+delta_in/2
+out_range = range_out[:-1]+delta_out/2
     
-plt.scatter(range_in[:-1]+delta_in/2, P_grado_in)
-plt.scatter(range_out[:-1]+delta_out/2, P_grado_out)
-plt.yscale('log')
-plt.xscale('log')
+ax2.scatter(in_range, P_grado_in)
+ax2.scatter(out_range, P_grado_out)
+
+ax2.plot(in_range, exponential(in_range, *popt_in))
+ax2.plot(out_range, exponential(out_range, *popt_out))
+
+ax2.set_yscale('log')
+ax2.set_xscale('log')
+
+ax2.set_ylim(1e-2,1)
+ax2.set_xlim(1e2, 8e2)
+
+ax2.set_xlabel('Grado de nodo')
+ax2.set_ylabel(r'$P_{k}$')
+
+#%%
+ax2.scatter(np.arange(41), Grado_in)
+ax2.scatter(np.arange(41), Grado_out)
 
 
-# %%
-plt.scatter(np.arange(41), Grado_in)
-plt.scatter(np.arange(41), Grado_out)
-# %%
+#%%
 
-def linear(x,m,n):
-    pass
